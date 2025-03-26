4cc6fdec2c842c9395639a6f559880f6491205ac
AL-Bench <shuaijiumei@126.com>
2025-03-26 21:49:56 +0800

    update: leadboard template

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/shuaijiumei/openrca_leadboard/',
+  base: '/openrca_leadboard/',
   plugins: [react()],
 })
